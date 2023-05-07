5cf217ec50f40b41dc7e0acb9383f6b89ab32dd3
0rmond <ormftaylor@gmail.com>
2023-05-07 13:00:11 +0100

    Following TJDeVries' tutorial to initialise plugin development and testing

---
+++ tests/example_spec.lua
@@ -0,0 +1,8 @@
+-- requires plenary to be installed
+-- :PlenaryBustedFile %
+
+describe("example test", function()
+    it("is possible to add two numbers", function()
+        return 1 + 1 == 2
+    end)
+end)
