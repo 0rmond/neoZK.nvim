a99223c0bf4b6be34f718ce5ddf198f6522b2c06
0rmond <ormftaylor@gmail.com>
2023-09-10 10:44:02 +0100

    Enabled quick opening of windows with some keymaps. NOTE: not final mappings! Worth implementing something like present neovim config's mappings

--- lua/neoZK/keymaps.lua
+++ lua/neoZK/keymaps.lua
@@ -3,8 +3,7 @@ local make_keymap = vim.keymap.set
 
 local mappings = {
     ["<S-z>"] = require("neoZK.window"),
-    ["<Esc>"] = require("neoZK.window.action_windows").close_window,
-}
+    ["<Esc>"] = require("neoZK.window.action_windows").close_window, }
 
 make_keymap('n', "<S-z>", function() require("neoZK.window").spawn_all_windows() end)
 make_keymap('n', "<Esc>", function() require("neoZK.window.action_windows").close_window() end)
