173e33bb1f753067836d43a80cbc7e9a8bd18753
0rmond <ormftaylor@gmail.com>
2023-09-10 10:30:51 +0100

    [#2] When resizing neovim, windows are removed and then opened with recalculated window sizes. Successive resizing no longer sees any lag. TODO: If the neovim terminal is too small, certain windows (eg. preview window) should be removed to fit main windows (search + results).

--- lua/neoZK/window/init.lua
+++ lua/neoZK/window/init.lua
@@ -2,14 +2,36 @@ local win_act = require("neoZK.window.action_windows")
 local api = vim.api
 local fun = require("fun")
 
-local function main()
+local function spawn_all_windows()
     local windows = require("neoZK.window.properties").generate_window_properties()
+
     fun.for_each(
         function(window)
             win_act.open_window(unpack(window))
         end,
         windows
     )
+    return fun.totable(fun.map(
+        function(window)
+            return window[1]
+        end,
+        windows
+    ))
+end
+
+
+local function main(window_id_to_close)
+
+    -- remove any previous triggers of VimResized to keep the next trigger fast
+    api.nvim_command("autocmd! VimResized")
+
+    if window_id_to_close then win_act.close_window(window_id_to_close) end
+
+    local window_ids = spawn_all_windows()
+    api.nvim_create_autocmd("VimResized", {
+        nested = true,
+        callback = function() main(window_ids[1]) end,
+    })
 end
 
 return { spawn_all_windows = main }
